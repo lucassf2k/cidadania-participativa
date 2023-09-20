b2895dad0ffa31345388975ec656d8a47a8bc92c
srSenhor <viniciusfeliciano14@gmail.com>
2023-09-20 01:22:28 -0300

    sei la to com sono

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,9 +1,12 @@
 import 'package:cidadania_participativa/controllers/ReportController.dart';
 import 'package:cidadania_participativa/core/button.dart';
+import 'package:cidadania_participativa/firebase/facade_firebase_service.dart';
 import 'package:cidadania_participativa/models/report.dart';
 import 'package:cidadania_participativa/firebase/facade_firebase_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../core/app_colors.dart';
 
@@ -15,7 +18,18 @@ class MenuPage extends StatefulWidget {
 
 class _MenuPageState extends State<MenuPage> {
   final fbFirestore = FacadeFirebaseFirestore();
+  final _firestoreService = FacadeFirebaseService();
+  FirebaseStorage fbStorage = FirebaseStorage.instance;
+
   late String str = "";
+  late String _url = "";
+  late List<Report> reps = [];
+
+  @override
+  void initState(){
+    super.initState();
+    loadReports();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +37,6 @@ class _MenuPageState extends State<MenuPage> {
 
     return DefaultTabController(
       length: 3,
-      //child: Obx( () => controller.existing_reports.value ? defined_reports() : undefined_reports()),
       child: Scaffold(
           appBar: AppBar(
             title: const Text("Cidadania Parcipativa"),
@@ -36,13 +49,34 @@ class _MenuPageState extends State<MenuPage> {
               ],
             ),
           ),
-          body: defined_reports(),
+          body: TabBarView(
+            children: [
+          ListView.builder(
+          itemCount: reps.length,
+            itemBuilder: (context, index) {
+              Report report = reps[index];
+              return Container(
+                child: Column(
+                  children: [
+                    _retornaImagem(''),
+                    Text('${reps[index].desc}'),
+                    Text('Postado em: ${reps[index].date.toString()}'),
+                    SizedBox(
+                      height: 30,
+                    )
+                  ],
+                )
+              );
+            },
+          ),
+              // Conteúdo da Aba 2
+              Center(child: Text('Conteúdo da Aba 2')),
+              // Conteúdo da Aba 3
+              Center(child: Text('Conteúdo da Aba 3')),
+            ],
+          ),
         floatingActionButton: Button('+', () => _onClickAddReport())
-        /*
-      * floatingActionButton: ElevatedButton(
-        onPressed: () => Get.toNamed('add_report_page'),
-      ),
-      * */
+
       )
     );
   }
@@ -50,30 +84,36 @@ class _MenuPageState extends State<MenuPage> {
   Future<void> _onClickAddReport() async{
     Report rep = Report();
     //str = await fbFirestore.createReport(rep);
+
     setState((){});
     
     Get.toNamed('add_report_page');
   }
-}
 
-TabBarView defined_reports(){
-  return TabBarView(
-      children: [
-        // Conteúdo da Aba 1
-        ListView(
-          children: [
-            Center(
-                child: Text('teste')
-            ),
-          ],
-        ),
-        // Conteúdo da Aba 2
-        Center(child: Text('Conteúdo da Aba 2')),
-        // Conteúdo da Aba 3
-        Center(child: Text('Conteúdo da Aba 3')),
-      ],
-    );
+  Future<void> loadReports() async {
+
+    List<Report> reports = await fbFirestore.readReport();
+
+    setState(() { reps = reports; });
   }
+
+  Future<void> _obterUrl(TaskSnapshot taskSnapshot, String fileName) async {
+    String url = await taskSnapshot.ref.getDownloadURL();
+    setState(() {
+      _url = url;
+    });
+  }
+
+  _retornaImagem (String str){
+    if(str.isEmpty){
+      return Image.network('gs://cidadnia-ativa.appspot.com/reports_photos/PHOTO-2021-05-21-19-22-54.jpg', fit:
+        BoxFit.cover,);
+    } else {
+      Image.network(str, fit:
+      BoxFit.cover);
+    }
+  }
+}
 
 
 /*
